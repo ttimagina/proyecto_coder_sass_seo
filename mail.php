7740c2659473fd83b64b587dbde16b28c5024ffa
ttimagina <martinfrette@gmail.com>
2022-03-12 18:55:20 -0300

    agregado de footer en el php

--- mail.php
+++ mail.php
@@ -76,6 +76,67 @@ echo'
         <p class="back color-contacto">Volver a Navegar <a clas="font_dancing color-contacto" href="index.html">a Navegar</a>.</p>
         
     </div>
+
+    <!--INCIO DE FOOTER--> 
+    <footer>
+      <div class="container-fluid linear-gradient1">
+          <div class="row py-2  text-center">
+              <div class="col-md-3 d-flex justify-content-center align-items-center mb-3"> <!--py-5 text-center-->
+                 
+                  <a href="index.html">                      
+                    <picture>
+                      <source media="(max-width: 975px)" srcset="img/small_logo1.svg">
+                      <source media="(min-width: 976px)" srcset="img/logo1.svg">
+                      <img src="img/logo1.svg" alt="logo ttimagina" class="img_logo_head">
+                    </picture>
+                  </a>
+                  
+              </div>
+              <div class="col-md-3 d-flex justify-content-center align-items-center mb-3"> <!--px-5 mb-3-->
+                  <section>    
+                    <h5 class="font_oswald">SECCIONES</h5>
+                      <ul class="nav flex-column">
+                        <li class="nav-item mb-2"><a href="branding.html" class="nav-link p-0 ">Branding</a></li>
+                        <li class="nav-item mb-2"><a href="animation.html" class="nav-link p-0 ">Animation</a></li>
+                        <li class="nav-item mb-2"><a href="website.html" class="nav-link p-0 ">Website</a></li>
+                        <li class="nav-item mb-2"><a href="contacto.html" class="nav-link p-0 ">Contacto</a></li>
+                      </ul>
+                  </section>    
+              </div>
+              <div class="col-md-3 d-flex justify-content-center align-items-center mb-3">
+                <section>
+                    <h5 class="font_oswald">CONTACTO</h5>
+                    <ul class="nav flex-column">
+                      <li class="nav-item mb-2"><h6 class="font_oswald">DIRECCION</h6>
+                        Luis Braille 3935</li>
+                      <li class="nav-item mb-2"><h6 class="font_oswald">CELULAR</h6>
+                        3794886995</li>
+                      <li class="nav-item mb-2"><h6 class="font_oswald">EMAIL</h6>
+                        martinfrette@gmail.com</li>
+                    </ul>
+                </section>    
+              </div>
+              <div class="col-md-3 d-flex justify-content-center align-items-center mb-3">
+                <section>
+                 <h5 class="font_oswald">SOCIAL MEDIA</h5>
+                      <ul class="nav flex-column">   
+                          <!-- <a href="https://www.instagram.com/ttimagina/" target="_blank" class="nav-link p-0 "><li><img class="footer_img" src="img/instagram (1).png" alt="instagram"> INSTAGRAM</li></a>
+                          <a href="https://www.facebook.com/martinfacundo/" target="_blank" class="nav-link p-0 "><li><img class="footer_img" src="img/facebook (1).png" alt="facebook"> FACEBOOK</li></a>-->
+                          <a href="https://www.linkedin.com/in/martinfrette/" target="_blank" class="nav-link p-0 "><li><img class="footer_img" src="img/linkedin.png" alt="linkedin"> LINKEDIN</li></a>
+                      </ul>
+                </section>      
+              </div>
+            </div> 
+      </div>
+      <div class="container-fluid bg-dark">
+          <div class="col d-flex justify-content-center color_white">
+          <p> © 2022 ttimagina, Inc. All rights reserved.</p> 
+          </div>
+      </div>
+  </footer>
+
+
+    <script src="js/bootstrap.js"></script> 
 </body>
 </html>
 
